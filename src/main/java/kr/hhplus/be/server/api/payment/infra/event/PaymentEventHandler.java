06f771bf97488a2c9703df769b9fedca34dba03f
DoGyunWoo <wo0170@naver.com>
2025-02-14 04:39:27 +0900

    feat: 이벤트 비동기 처리

--- src/main/java/kr/hhplus/be/server/api/payment/infra/event/PaymentEventHandler.java
+++ src/main/java/kr/hhplus/be/server/api/payment/infra/event/PaymentEventHandler.java
@@ -2,15 +2,17 @@ package kr.hhplus.be.server.api.payment.infra.event;
 
 import kr.hhplus.be.server.api.mock.service.MockDataPlatformService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
-
 public class PaymentEventHandler {
     private final MockDataPlatformService mockDataPlatformService;
+
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void paymentCompleteEventHandler(PaymentCompleteEvent event) {
         String message = String.format("사용자(UserId: %d) 결제 성공!", event.getUserId());
